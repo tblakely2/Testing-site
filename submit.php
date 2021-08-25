35567eb47c29b11d0bd684a61c65f91a84edf699
Timothy James Blakely <tblakely2@studentmail.nait.ca>
2021-08-25 14:50:51 -0600

    changed from gmail to work address

--- submit.php
+++ submit.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit']))  {
 	$mailFrom = $_POST['mail'];  
 	$message = $_POST['message'];  
 
-	$mailTo = "tjblakely@gmail.com";
+	$mailTo = "tblakely@profireenergy.com";
 	$headers = "From: ".$mailFrom;
 	$txt = "You have received an email from ".$name.".\n\n".$message;
 

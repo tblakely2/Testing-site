04f46840ec0df15a94d81dbda0e5a81f7e093fa5
Timothy James Blakely <tblakely2@studentmail.nait.ca>
2021-08-25 14:46:04 -0600

    still figuring names out

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
-<?php 
-//if "email" variable is filled out, send email   
-if (isset($_POST['submit']))  {    
-	//email information
-	$name = $_POST['name'];  
-	$subject = $_POST['subject'];  
-	$mailFrom = $_POST['mail'];  
-	$message = $_POST['message'];  
-
-	$mailTo = "tjblakely@gmail.com";
-	$headers = "From: ".$mailFrom;
-	$txt = "You have received an email from ".$name.".\n\n".$message;
-
-	mail($mailTo, $subject, $txt, $headers);
-	header("Location: index.html?mailsend");
-} 
-?>
+<!-- Sample HTML Form -->
+<form class="customer-profile-form" action="submit.php" method="post">
+    <input type="text" name="name" placeholder="Full name">
+    <input type="text" name="mail" placeholder="Your email">
+    <input type="text" name="subject" placeholder="Subject">
+    <textarea name="message" placeholder="Message"></textarea>
+    <button type="submit" name="submit">Send</button>
+</form>
+    
---
+++ submit.php
@@ -0,0 +1,17 @@
+<?php 
+//if "email" variable is filled out, send email   
+if (isset($_POST['submit']))  {    
+	//email information
+	$name = $_POST['name'];  
+	$subject = $_POST['subject'];  
+	$mailFrom = $_POST['mail'];  
+	$message = $_POST['message'];  
+
+	$mailTo = "tjblakely@gmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have received an email from ".$name.".\n\n".$message;
+
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.php?mailsend");
+} 
+?>
